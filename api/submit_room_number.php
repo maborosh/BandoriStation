1e529bd0e8ea507228939cd509432defb664e231
maborosh <47907224+maborosh@users.noreply.github.com>
2019-09-15 11:14:00 -0700

    Update submit_room_number.php

--- api/submit_room_number.php
+++ api/submit_room_number.php
@@ -8,20 +8,30 @@ function submit_room_number()
             $room_number_array['number'] = $_GET['number'];
         } else {
             return array(
-                'status' => 'illegal_number_format'
+                'status' => 'failure',
+                'response' => 'illegal_number_format'
             );
         }
     } else {
         return array(
-            'status' => 'lack_number_parameter'
+            'status' => 'failure',
+            'response' => 'lack_number_parameter'
         );
     }
 
     if (isset($_GET['user_id'])) {
-        $room_number_array['user_id'] = $_GET['user_id'];
+        if (is_numeric($_GET['user_id'])) {
+            $room_number_array['user_id'] = $_GET['user_id'];
+        } else {
+            return array(
+                'status' => 'failure',
+                'response' => 'illegal_user_id_formal'
+            );
+        }
     } else {
         return array(
-            'status' => 'lack_user_id_parameter'
+            'status' => 'failure',
+            'response' => 'lack_user_id_parameter'
         );
     }
 
@@ -29,7 +39,8 @@ function submit_room_number()
         $room_number_array['raw_message'] = urldecode(trim($_GET['raw_message']));
     } else {
         return array(
-            'status' => 'lack_raw_message_parameter'
+            'status' => 'failure',
+            'response' => 'lack_raw_message_parameter'
         );
     }
 
@@ -38,24 +49,28 @@ function submit_room_number()
             $room_number_array['source'] = $_GET['source'];
         } else {
             return array(
-                'status' => 'unregistered_data_source'
+                'status' => 'failure',
+                'response' => 'unregistered_data_source'
             );
         }
     } else {
         return array(
-            'status' => 'lack_source_parameter'
+            'status' => 'failure',
+            'response' => 'lack_source_parameter'
         );
     }
 
     if (isset($_GET['token'])) {
         if (!data_source_check($_GET['source'], 2, $_GET['token'])) {
             return array(
-                'status' => 'invalid_token'
+                'status' => 'failure',
+                'response' => 'invalid_token'
             );
         }
     } else {
         return array(
-            'status' => 'lack_token_parameter'
+            'status' => 'failure',
+            'response' => 'lack_token_parameter'
         );
     }
 
@@ -70,30 +85,31 @@ function submit_room_number()
             $room_number_array['type'] = '7万常规房';
         } else {
             return array(
-                'status' => 'undefined_type_value'
+                'status' => 'failure',
+                'response' => 'undefined_type_value'
             );
         }
     } else {
         if (
             stristr($room_number_array['raw_message'], '25w') or
-            strstr($room_number_array['raw_message'], '25万')
+            stristr($room_number_array['raw_message'], '25万')
         ) {
             $room_number_array['type'] = '25万房';
         } elseif (
             stristr($room_number_array['raw_message'], '18w') or
-            strstr($room_number_array['raw_message'], '18万') or
+            stristr($room_number_array['raw_message'], '18万') or
             strstr($room_number_array['raw_message'], '大师房')
         ) {
             $room_number_array['type'] = '18万大师房';
         } elseif (
             stristr($room_number_array['raw_message'], '12w') or
-            strstr($room_number_array['raw_message'], '12万') or
+            stristr($room_number_array['raw_message'], '12万') or
             strstr($room_number_array['raw_message'], '高手房')
         ) {
             $room_number_array['type'] = '12万高手房';
         } elseif (
             stristr($room_number_array['raw_message'], '7w') or
-            strstr($room_number_array['raw_message'], '7万') or
+            stristr($room_number_array['raw_message'], '7万') or
             strstr($room_number_array['raw_message'], '常规房')
         ) {
             $room_number_array['type'] = '7万常规房';
@@ -109,24 +125,26 @@ function submit_room_number()
     $redis_key = 'bandori_room_number';
     $redis_number_data = $redis->lRange($redis_key, 0, -1);
     $push_flag = true;
-    for ($i = 0; $i < count($redis_number_data); $i++) {
+    for ($i = count($redis_number_data) - 1; $i >= 0; $i--) {
         $room_number_array_temp = json_decode($redis_number_data[$i], true);
-        if ($timestamp - $room_number_array_temp['time'] > 120000) {
-            $redis->lPop($redis_key);
-        } elseif ($timestamp - $room_number_array_temp['time'] < 10000) {
+        if ($timestamp - $room_number_array_temp['time'] < 10000) {
             if ($room_number_array['number'] == $room_number_array_temp['number']) {
                 $push_flag = false;
             }
+        } else {
+            break;
         }
     }
     if ($push_flag) {
         $redis->rPush($redis_key, json_encode($room_number_array));
         return array(
-            'status' => 'success'
+            'status' => 'success',
+            'response' => ''
         );
     } else {
         return array(
-            'status' => 'failure'
+            'status' => 'failure',
+            'response' => 'duplicate_number_submit'
         );
     }
 }
