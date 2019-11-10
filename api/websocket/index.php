6a88eb4a09b96a3b55b81dc50986fd3a3717c510
maborosh <47907224+maborosh@users.noreply.github.com>
2019-11-09 22:20:26 -0700

    Add files via upload

--- api/websocket/index.php
+++ api/websocket/index.php
@@ -1,21 +1,56 @@
 <?php
 define('API_ROOT', dirname(dirname(__FILE__)));
+define('HEARTBEAT_TIME', 30);
+
 require 'Workerman/Autoloader.php';
 require API_ROOT . '/other_functions.php';
 require API_ROOT . '/query_room_number.php';
+
+use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
 use Workerman\Lib\Timer;
 
 $worker = new Worker('websocket://127.0.0.1:9000');
-// 进程启动后定时推送数据给客户端
-$worker->onWorkerStart = function($worker) {
-    Timer::add(0.5, function() use($worker) {
-        $data = query_room_number();
-        if ($data['response']) {
-            foreach($worker->connections as $connection) {
-                $connection->send(json_encode($data));
+
+// 首次连接发送消息
+$worker->onConnect = function (TcpConnection $connection)
+{
+    $query_result = query_room_number();
+    if ($query_result['status'] == 'success' and $query_result['response']) {
+        $connection->send(json_encode($query_result));
+    }
+};
+
+$worker->onMessage = function($connection) {
+    $connection->lastMessageTime = time();
+};
+
+// 持续推送消息
+$latest_time = 0;
+$redis = new Redis();
+$redis->pconnect('127.0.0.1');
+$worker->onWorkerStart = function($worker) use (&$latest_time, $redis)
+{
+    Timer::add(0.5, function() use($worker, &$latest_time, $redis) {
+        $query_result = query_room_number($latest_time);
+        $time_now = time();
+        if ($query_result['status'] == 'success' and $query_result['response']) {
+            $latest_time = $query_result['response'][count($query_result['response']) - 1]['time'];
+            $send_flag = true;
+        } else {
+            $send_flag = false;
+        }
+        foreach($worker->connections as $connection) {
+            if (empty($connection->lastMessageTime)) {
+                $connection->lastMessageTime = $time_now;
+            }
+            if ($time_now - $connection->lastMessageTime > HEARTBEAT_TIME) {
+                $connection->close();
+            } elseif ($send_flag) {
+                $connection->send(json_encode($query_result));
             }
         }
+        $redis->set('bandori_station_online_number', count($worker->connections));
     });
 };
 
